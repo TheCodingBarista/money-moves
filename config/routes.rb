c4e4018f4f7b52ccf1858c17474b297f7425c3ba
melissa_holt <melissa.holt21@gmail.com>
2020-07-25 21:10:11 -0700

    stubbed out search feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :spotify do
+      get 'episodes/search'
+    end
+  end
+  
   devise_for :users, path: '/', path_names: {sign_in: 'login', sign_out: 'logout', registration: 'register' }, controllers: { omniauth_callbacks: 'omniauth' }
   
   root to: "users#welcome"
